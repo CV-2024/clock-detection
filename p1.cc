49c2cd8f61d34711260ca443debcd0e19b7c1e41
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2024-04-21 04:40:58 -0400

    added Canny and started HoughLine

--- p1.cc
+++ p1.cc
@@ -45,7 +45,40 @@ int main()
                 // circle(grayImage, center, radius, Scalar(0, 255, 0), 2); // white 
                 circle(img, center, radius, Scalar(0, 255, 0), 2); // nean green 
             }
+
+            // step 4: Use CANNY for line
+            Mat edges;
+            Canny(grayImage, edges, 50, 100, 3, true);
+            imshow("canvasOutput", edges);
+            // int k1 = waitKey(0); // Wait for a keystroke in the window
+
+            // Step 5: Use Hough Line Transform to detect lines
+            vector<Vec2f> lines;
+            HoughLines(edges, lines, 1, CV_PI / 180, 40, 0, 0, 0, CV_PI);
+
             
+            // Step 6: Draw detected lines on the original image
+            if (lines.size() >= 2){
+                cout << "MIN 2 lines detected!" << endl;
+                for (size_t i = 0; i < lines.size(); i++) {
+                    float rho = lines[i][0], theta = lines[i][1];
+                    Point pt1, pt2;
+                    double a = cos(theta), b = sin(theta);
+                    double x0 = a * rho, y0 = b * rho;
+                    pt1.x = cvRound(x0 + 1000 * (-b));
+                    pt1.y = cvRound(y0 + 1000 * (a));
+                    pt2.x = cvRound(x0 - 1000 * (-b));
+                    pt2.y = cvRound(y0 - 1000 * (a));
+                    line(img, pt1, pt2, Scalar(0, 0, 255), 2, LINE_AA);
+                }
+            }
+            else{
+                // Step 6b: Increase radius and go back to step 2 (min of 2 lines not dectected)
+                cout << "MIN 2 lines NOT detected!" << endl;
+                radius++;
+                continue;
+            }
+
             // other steps ...
 
             // Break the loop if both circles and lines are detected
