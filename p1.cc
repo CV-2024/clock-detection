b5a49ec235103c07d5ced129bd1b9a58851b4b2b
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2024-04-21 05:24:34 -0400

    added Probabilistic Line Transform

--- p1.cc
+++ p1.cc
@@ -6,11 +6,10 @@
 using namespace std;
 using namespace cv;
 
-int main()
-{
+int main(){
     // Step 1: input image
     string image_path = "data_analog_clocks/samples/1.jpg";
-    // string image_path = "data_analog_clocks/real_staright_images/fake_hands/1-00/10.jpg";
+    // string image_path = "data_analog_clocks/real_staright_images/fake_hands/1-00/10.jpg"; // with background 
     // string image_path = "data_analog_clocks/real_staright_images/real_hands/no_background/0_3_07.pgm";
     // string image_path = "data_analog_clocks/real_staright_images/real_hands/no_background/1_0_10.jpg";
     ClockDetection clockDetector(image_path);
@@ -48,40 +47,56 @@ int main()
 
             // step 4: Use CANNY for line
             Mat edges;
-            Canny(grayImage, edges, 50, 100, 3, true);
+            Canny(grayImage, edges, 50, 200, 3, true);
             imshow("canvasOutput", edges);
 
             // Step 5: Use Hough Line Transform to detect lines
-            vector<Vec2f> lines;
-            HoughLines(edges, lines, 1, CV_PI / 180, 40, 0, 0, 0, CV_PI);
 
+            // Standard Hough Line Transform
+            vector<Vec2f> lines; // will hold the results of the detection
+            HoughLines(edges, lines, 1, CV_PI/180, 50, 0, 0); // runs the actual detection
             
-            // Step 6: Draw detected lines on the original image
+            // // Probabilistic Line Transform
+            // vector<Vec4i> linesP; 
+            // HoughLinesP(edges, linesP, 1, CV_PI/180, 50, 50, 10 );
+
+
+ 
+            // // Step 6: Draw detected lines on the original image
+            // if (linesP.size() >= 2){
+            //     cout << "linesP.size(): " << linesP.size() << endl;
+            //     cout << "MIN 2 lines detected!" << endl;
+            //     for (size_t i = 0; i < linesP.size(); i++) {
+            //         Vec4i l = linesP[i];
+            //         line( img, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+            //     }
+            // }
             if (lines.size() >= 2){
+                cout << "lines.size(): " << lines.size() << endl;
                 cout << "MIN 2 lines detected!" << endl;
-                for (size_t i = 0; i < lines.size(); i++) {
+                for( size_t i = 0; i < lines.size(); i++ ){
                     float rho = lines[i][0], theta = lines[i][1];
                     Point pt1, pt2;
                     double a = cos(theta), b = sin(theta);
-                    double x0 = a * rho, y0 = b * rho;
-                    pt1.x = cvRound(x0 + 1000 * (-b));
-                    pt1.y = cvRound(y0 + 1000 * (a));
-                    pt2.x = cvRound(x0 - 1000 * (-b));
-                    pt2.y = cvRound(y0 - 1000 * (a));
-                    line(img, pt1, pt2, Scalar(0, 0, 255), 2, LINE_AA);
+                    double x0 = a*rho, y0 = b*rho;
+                    pt1.x = cvRound(x0 + 1000*(-b));
+                    pt1.y = cvRound(y0 + 1000*(a));
+                    pt2.x = cvRound(x0 - 1000*(-b));
+                    pt2.y = cvRound(y0 - 1000*(a));
+                    line(img, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+                    // Break the loop if both circles and lines are detected
                 }
+                break;
             }
             else{
                 // Step 6b: Increase radius and go back to step 2 (min of 2 lines not dectected)
                 cout << "MIN 2 lines NOT detected!" << endl;
                 radius++;
-                continue;
+                // continue;
             }
 
             // other steps ...
 
-            // Break the loop if both circles and lines are detected
-            break;
         }
         else{
             // Step 3b: Increase radius and go back to step 2 (no cicle dectected)            
