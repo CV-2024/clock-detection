c0b7106b8a2454c1978d446d278938e796ba2dc7
Sumia Hago <sumia.hago15@myhunter.cuny.edu>
2024-04-23 04:37:29 -0400

    drawing function

--- ClockDetection.cc
+++ ClockDetection.cc
@@ -15,3 +15,27 @@ vector<Vec3f> ClockDetection::detectCircles(const cv::Mat& grayImage, int dp, in
     return circles;
 }
 
+
+void ClockDetection::drawDetectCirclesCopy(vector<Vec3f> circles, const cv::Mat& grayImage){
+    //DRAWS RED CIRCLE ON DETECTED CIRCLE:
+    cv::Mat colorImage;
+
+    cv::cvtColor(grayImage, colorImage, cv::COLOR_GRAY2BGR);
+    for (const auto& circle : circles) {
+        cv::Point center(cvRound(circle[0]), cvRound(circle[1]));
+        int radius = cvRound(circle[2]);
+        cv::circle(colorImage, center, radius, cv::Scalar(0, 0, 255), 2); //draw red circle on gray image
+    }
+
+    // //testing image output:
+    imshow("Draw Copy", colorImage);
+    int n = waitKey(0); // Wait for a keystroke in the window
+}
+
+void ClockDetection::drawDetectCircles(vector<Vec3f> circles, const cv::Mat& Image){
+    for (const auto& circle : circles) {
+        cv::Point center(cvRound(circle[0]), cvRound(circle[1]));
+        int radius = cvRound(circle[2]);
+        cv::circle(Image, center, radius, cv::Scalar(0, 0, 255), 2); //draw red circle on gray image
+    }
+}
